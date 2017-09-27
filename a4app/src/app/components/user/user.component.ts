9593727fe078f515ae97ef03f261d61857e0c0e3
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-09-27 15:23:16 -0400

    included forms, services etc

--- a4app/src/app/components/user/user.component.ts
+++ a4app/src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService} from "../../services/data.service";
+import {post} from "selenium-webdriver/http";
 
 @Component({
   selector: 'app-user',
@@ -6,40 +8,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  name:string = 'Steve Smith';
+  name:string;
   age:number;
   email:string;
   address:Address;
   hobbies:string[];
   hello:any;
+  posts:Post[];
+  isEdit:boolean = false;
 
-  constructor() {
-    console.log('constructor ran...');
+  constructor(private dataService:DataService) {
+      console.log('constructor ran...');
   }
 
   ngOnInit() {
-    console.log('ngOnInit ran...')
+      console.log('ngOnInit ran...');
+      this.name = 'John Doe';
+      this.email = 'johndoe123@gmail.com';
+      this.age = 30;
+      this.address = {
+          street:'50 Main st',
+          city:'Boston',
+          state:'MA'
+      }
+      this.hobbies = ['write code', 'watch movies', 'listen to music'];
+      this.hello = 'hello';
 
-    this.name = 'John Doe';
-    this.age = 21;
-    this.address = {
-      street: 'Park Avenue',
-      city: 'Boston',
-      state: 'LA'
-    }
-    this.hobbies = ['Write code', 'watch movies', 'listen to music'];
-    this.hello = 'Hello';
+      this.dataService.getPosts().subscribe((posts) => {
+        // console.log(posts);
+          this.posts = posts;
+      });
   }
 
-  onclick() {
-    this.name = 'musk_1107';
-    this.hobbies.push('New hobby');
+  onClick() {
+      this.name = 'Chiranjeeb Mahanta';
+      this.hobbies.push('GSoCer');
+      this.hobbies.push('Roots Core Team Member');
+  }
+
+  addHobby(hobby)   {
+      console.log(hobby);
+      this.hobbies.unshift(hobby);
+      return false;
+  }
+
+  deleteHobby(hobby)    {
+      for(let i = 0; i<this.hobbies.length; i++)    {
+          if(this.hobbies[i] == hobby)  {
+              this.hobbies.splice(i, 1);
+          }
+      }
+  }
+
+  toggleEdit()  {
+      this.isEdit = !this.isEdit;
   }
 
 }
 
-interface Address {
-  street:string,
-  city:string,
-  state:string
+interface Address   {
+    street:string,
+    city:string,
+    state:string
+}
+
+interface Post  {
+    id: number,
+    title:string,
+    body:string,
+    userId:number
 }
